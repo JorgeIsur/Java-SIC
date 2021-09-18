b9db595c5112114af8426370dbe0f536edd961d7
JorgeIsur <ramirezisur@gmail.com>
2021-09-17 19:20:39 -0500

    zzzz temperatura

---
+++ demo/src/main/java/com/temperatura/Temperatura.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.temperatura;
+
+import com.pi4j.Pi4J;
+import com.pi4j.context.Context;
+import com.pi4j.io.gpio.digital.DigitalOutput;
+import com.pi4j.io.gpio.digital.DigitalState;
+import com.pi4j.platform.Platforms;
+import com.pi4j.util.Console;
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ *
+ * @author Isur
+ */
+public class Temperatura {
+
+    
+    public static void main(String[] args) throws Exception {
+     
+
+}
